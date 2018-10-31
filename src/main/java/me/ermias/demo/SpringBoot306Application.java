24e2ecb77ecc1d68450a04666ee1e8cf139de93f
Ermiji <eghebrez@gmail.com>
2018-10-30 22:18:27 -0400

    One to Many springboot example

---
+++ src/main/java/me/ermias/demo/SpringBoot306Application.java
@@ -0,0 +1,12 @@
+package me.ermias.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot306Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot306Application.class, args);
+    }
+}
